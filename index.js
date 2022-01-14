3125b556ee56b5f90c553363a816b607b8d4ee5e
Steve Schaner <schanerst@gmail.com>
2022-01-14 18:04:00 -0500

    Portfolio projects are now linked to a json file.

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const nav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
+const carouselImage1 = document.querySelector('#carousel-image-1');
+const carouselImage2 = document.querySelector('#carousel-image-2');
+const carouselImage3 = document.querySelector('#carousel-image-3');
+const portfolioTitle = document.querySelector('.portfolio-info--title');
+const portfolioContent = document.querySelector('.portfolio-info--content');
 
 navToggle.addEventListener('click', navigationToggle);
 
-tabList.addEventListener('keydown', changeTabFocus);
-
 tabs.forEach((tabs) => {
-  tabs.addEventListener('click', changeTabPanel);
+  tabs.addEventListener('click', getProjectInformation);
 });
+
+tabList.addEventListener('keydown', changeTabFocus);
 
 function navigationToggle() {
   const visibility = nav.getAttribute('data-visible');
@@ -46,28 +51,38 @@ function changeTabFocus(e) {
   }
 }
 
-function changeTabPanel(e) {
+function getProjectInformation(e) {
   const targetTab = e.target;
-  console.log(targetTab);
-  const targetPanel = targetTab.getAttribute('aria-controls');
-  console.log(targetPanel);
-  const targetImage = targetTab.getAttribute('data-image');
-  console.log(targetImage);
+  const targetTabIndex = targetTab.getAttribute('tabindex');
+  fetch('data.json')
+    .then((response) => response.json())
+    .then((data) => {
+      portfolioTitle.innerHTML = data.projects[targetTabIndex].title;
+      portfolioContent.innerHTML = data.projects[targetTabIndex].description;
+      carouselImage1.src = data.projects[targetTabIndex].images[0];
+      carouselImage2.src = data.projects[targetTabIndex].images[1];
+      carouselImage3.src = data.projects[targetTabIndex].images[2];
+    });
+  // console.log(targetTabIndex);
+  // const targetPanel = targetTab.getAttribute('aria-controls');
+  // console.log(targetPanel);
+  // const targetImage = targetTab.getAttribute('data-image');
+  // console.log(targetImage);
 
-  const tabContainer = targetTab.parentNode;
-  const mainContainer = tabContainer.parentNode;
+  // const tabContainer = targetTab.parentNode;
+  // const mainContainer = tabContainer.parentNode;
 
-  tabContainer
-    .querySelector('[aria-selected="true"]')
-    .setAttribute('aria-selected', false);
+  // tabContainer
+  //   .querySelector('[aria-selected="true"]')
+  //   .setAttribute('aria-selected', false);
 
-  targetTab.setAttribute('aria-selected', true);
+  // targetTab.setAttribute('aria-selected', true);
 
-  hideContent(mainContainer, '[role="tabpanel"]');
-  showContent(mainContainer, [`#${targetPanel}`]);
+  // hideContent(mainContainer, '[role="tabpanel"]');
+  // showContent(mainContainer, [`#${targetPanel}`]);
 
-  hideContent(mainContainer, '[role="carouselImage"]');
-  showContent(mainContainer, [`#${targetImage}`]);
+  // hideContent(mainContainer, '[role="carouselImage"]');
+  // showContent(mainContainer, [`#${targetImage}`]);
 }
 
 function hideContent(parent, content) {
